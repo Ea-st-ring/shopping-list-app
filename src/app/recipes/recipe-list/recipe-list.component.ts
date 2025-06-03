5f58f4203ad69e9f3abe0a9ca3636389b54d76fe
Donghyun Hwang <eastring@elicer.com>
2025-06-04 01:29:36 +0900

    feat: add recipe components skeleton logics

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-list',
   standalone: false,
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://kurlylog-img.kurly.com/hdims/resize/%3E720x%3E720/quality/85/src/recipe/kimchi-army-base-stew-recipe-step-8.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://static.wtable.co.kr/image/production/service/recipe/2431/70a672e4-d3f5-46ee-aef8-fb928ed52bda.jpg?size=800x800'
+    ),
+  ];
 
+  ngOnInit(): void {}
 }
