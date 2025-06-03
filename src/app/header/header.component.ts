21dc318156b4cf35adfea352dce5305180b58cf7
Donghyun Hwang <eastring@elicer.com>
2025-06-04 01:06:44 +0900

    feat: publish header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   standalone: false,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  collapsed = true;
+}
